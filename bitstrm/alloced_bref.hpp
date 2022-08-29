5e59dfb4daa621c7cb18196ba3f727868d5cfc98
chris <chris@durso.org>
2022-08-29 12:18:16 -0700

    alloced_bref abandon reserved section and added copy

--- bitstrm/alloced_bref.hpp
+++ bitstrm/alloced_bref.hpp
@@ -13,64 +13,41 @@ namespace bitstrm {
   //
   // a heap re/allocate for bitstrm
   //
-  // maintains a seperate 'reserved' section 
-  //
-  // pre()[reserved-bits]|{this->bref}
-  
+
   class alloced_bref : public bref {
   public:
      
     // alloced_bref
     //
-    // allocate at least main_bsize + pre_reserved space, sets this
-    // location following reserved bits
-
-    alloced_bref(ureg main_bsize, ureg pre_reserved = 0)
-      : m_reserved(pre_reserved){allocate(main_bsize);}
-    alloced_bref(): m_reserved(0) {allocate(0);}
+    // allocate at least bsz bits
+    alloced_bref(ureg bsz){allocate(bsz);}
+    alloced_bref(){allocate(0);}
     alloced_bref(alloced_bref&& rhs) = default;
     alloced_bref& operator=(alloced_bref&& rhs) = default;
-    
-    
-    // set_reserve
-    //
-    // sets the reserved bsize
-    void set_reserve(ureg reserved){ m_reserved = reserved;}
-
-    // get_reserved
-    //
-    // gets the reserved bsize
-    ureg get_reserve()const { return m_reserved; }
-    
+        
     // swap
     //
     // non allocating swap
     void swap(alloced_bref& rhs){
       std::swap(static_cast<bref&>(*this), static_cast<bref&>(rhs));
-      std::swap(m_reserved, rhs.m_reserved);
       m_buf.swap(rhs.m_buf);
     }
     
     // allocate
     //
-    // allocate or reallocate bsize + reserved bits
-    void allocate(ureg bsize) {
-      m_bsize = bsize + m_reserved;
+    // allocate at least new_bsize bits invalidates any prior bref to this
+    // memory, reset bref to beginning of this buffer
+    void allocate(ureg new_bsize) {
+      m_bsize = new_bsize;
       m_buf.resize(uregs(m_bsize));
       reset();
     } 
     
     // reset
     //
-    // resets position to begining of buffer *following* reserve
+    // resets position to begining of buffer
     void reset(){
-      this->bref::operator=(pre() + m_reserved);
-    }
-
-    // pre
-    //
-    bref pre(){
-      return bref(data());
+      this->bref::operator=(bref(data()));
     }
 
     // zero
@@ -96,9 +73,18 @@ namespace bitstrm {
   private:
     std::vector<ureg> m_buf;
     ureg              m_bsize;
-    ureg              m_reserved;
   };
 
+
+  // copy
+  //
+  // copies the content of src (ignoring current position)  into dest
+  // where dest must be sized to at least src.bsize()
+  inline bref copy(alloced_bref const& src, bref dest){
+    bref const bbeg(const_cast<char*>(src.data()));
+    return bitstrm::copy(bbeg, bbeg + src.bsize(), dest);
+  }
+  
 } 
 
 
